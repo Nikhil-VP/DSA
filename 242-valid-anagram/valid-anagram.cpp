5ebb2063cd15927a5692ea15a335e8cc7af43476
Nikhil V P <nikhilvp888@gmail.com>
2026-08-12 11:49:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (92.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)
+        // Quick check: if lengths differ, they can't be anagrams
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int count[26] = {0};
+
+        // Increment count for characters in s, decrement for t
+        for (int i = 0; i < s.length(); ++i) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        // Check if all counts are zero
+        for (int i = 0; i < 26; ++i) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+
         return true;
-        else
-        return false;
     }
 };
