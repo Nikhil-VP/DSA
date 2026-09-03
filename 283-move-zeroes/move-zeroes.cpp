a3b366a5a7d4e9adccfe7d10e6e462e79b6bd966
Nikhil V P <nikhilvp888@gmail.com>
2026-09-03 20:33:12 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (55.89%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,18 +1,27 @@
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int count= 0;
+//         for(int i=0;i < nums.size();i++){
+//             if(nums[i] == 0){
+//                 count++;
+//                 nums.erase(nums.begin()+i);
+//             }
+
+//         }
+//         while(count!=0){
+//         nums.push_back(0);
+//         count--;
+//        }
+//     }
+// };
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int count= 0;
-        for(int i=0;i < nums.size();i++){
-            if(nums[i] == 0){
-                count++;
-                nums.erase(nums.begin()+i);
-                i--;
+        for (int insertPos = 0, i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[insertPos++], nums[i]);
             }
-
         }
-        while(count!=0){
-        nums.push_back(0);
-        count--;
-       }
     }
 };
