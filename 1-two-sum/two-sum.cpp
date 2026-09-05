1f2da7b62c74d6091bad9bac0e7ae66887135e96
Nikhil V P <nikhilvp888@gmail.com>
2026-09-05 11:10:25 +0530

    Time: 4 ms (52.83%) | Memory: 14.8 MB (43.96%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,20 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int i,j;int req;
+//         vector<int> arr(2);
+//         for(i=0;i < nums.size();i++)
+//         {
+//             req = target - nums[i];
+//             for(j=i+1; j<nums.size();j++)
+//             {
+//                 if(req == nums[j])
+//                 {
+//                     arr[0] = i;
+//                     arr[1] = j;
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+// };
+#include <vector>
+#include <unordered_map>
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int i,j;int req;
-        vector<int> arr(2);
-        for(i=0;i < nums.size();i++)
-        {
-            req = target - nums[i];
-            for(j=i+1; j<nums.size();j++)
-            {
-                if(req == nums[j])
-                {
-                    arr[0] = i;
-                    arr[1] = j;
-                }
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> num_map; 
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+    
+            if (num_map.find(complement) != num_map.end()) {
+                return {num_map[complement], i};
             }
+ 
+            num_map[nums[i]] = i;
         }
-        return arr;
+        
+        return {};
     }
 };
