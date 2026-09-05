c8d119822efa25a0d9753a439cf0d2a0e6cf08fd
Nikhil V P <nikhilvp888@gmail.com>
2026-09-05 11:45:38 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (52.55%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,47 +1,47 @@
-// class Solution {
-// public:
-//     int maxSubArray(vector<int>& nums) {
-//         int maxs = INT_MIN,sum = 0;
-//         for(int i=0;i < nums.size();i++){
-//             sum += nums[i];
-//             if(sum > maxs){
-//                 maxs = sum;
-//             }
-//             if(sum < 0){
-//                 sum = 0;
-//             }
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxs = INT_MIN,sum = 0;
+        for(int i=0;i < nums.size();i++){
+            sum += nums[i];
+            if(sum > maxs){
+                maxs = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
 
-//         }
-//         return maxs;
-//     }
-// };
-class Solution{
-    public:
-         int maxSubArray(vector<int>& nums) {
-           return divideandconquer(nums , 0 ,nums.size()-1); 
-         }
-    private:
-        int divideandconquer(vector<int>& nums,int left,int right){
-            if(left == right) return nums[left];
-            int mid = left +(right - left)/2;
-            int left_max= divideandconquer(nums , left , mid);
-            int right_max= divideandconquer(nums , mid+1 , right);
-            int cross_max = maxcrossingsum(nums, left, mid, right);
-            return max({left_max,right_max,cross_max});
         }
-        int maxcrossingsum(vector <int>& nums,int left,int mid,int right){
-            int sum=0;
-            int left_sum = INT_MIN;
-            for(int i =mid;i>= left;i--){
-                sum += nums[i];
-                left_sum = max(left_sum,sum);
-            }
-            sum = 0;
-            int right_sum = INT_MIN;
-            for(int i=mid+1; i <=right;i++){
-                sum += nums[i];
-                right_sum = max(right_sum,sum);
-            }
-            return left_sum + right_sum;
-        }
+        return maxs;
+    }
 };
+// class Solution{
+//     public:
+//          int maxSubArray(vector<int>& nums) {
+//            return divideandconquer(nums , 0 ,nums.size()-1); 
+//          }
+//     private:
+//         int divideandconquer(vector<int>& nums,int left,int right){
+//             if(left == right) return nums[left];
+//             int mid = left +(right - left)/2;
+//             int left_max= divideandconquer(nums , left , mid);
+//             int right_max= divideandconquer(nums , mid+1 , right);
+//             int cross_max = maxcrossingsum(nums, left, mid, right);
+//             return max({left_max,right_max,cross_max});
+//         }
+//         int maxcrossingsum(vector <int>& nums,int left,int mid,int right){
+//             int sum=0;
+//             int left_sum = INT_MIN;
+//             for(int i =mid;i>= left;i--){
+//                 sum += nums[i];
+//                 left_sum = max(left_sum,sum);
+//             }
+//             sum = 0;
+//             int right_sum = INT_MIN;
+//             for(int i=mid+1; i <=right;i++){
+//                 sum += nums[i];
+//                 right_sum = max(right_sum,sum);
+//             }
+//             return left_sum + right_sum;
+//         }
+// };
